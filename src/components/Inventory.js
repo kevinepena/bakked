7f1a8f0e0039fd95bc47483a239e839867586be4
kevinpena <kep85@txstate.edu>
2021-02-02 17:13:32 -0600

    format

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,18 +1,24 @@
-import React, { Component } from 'react';
-import AddItemForm from './AddItemForm';
-import EditItem from './EditItem';
-
+import React, { Component } from 'react'
+import AddItemForm from './AddItemForm'
+import EditItem from './EditItem'
 
 export default class Inventory extends Component {
-    render() {
-
-        return (
-            <div className="inventory">
-                <h2>Inventory</h2>
-                {Object.keys(this.props.items).map(key => <EditItem key={key} llave={key} item={this.props.items[key]} updateItem={this.props.updateItem} deleteItem={this.props.deleteItem} />)}
-                <AddItemForm addItem={this.props.addItem} />
-                {/* <button onClick={this.loadItems}></button> */}
-            </div>
-        )
-    }
+  render () {
+    return (
+      <div className='inventory'>
+        <h2>Inventory</h2>
+        {Object.keys(this.props.items).map(key => (
+          <EditItem
+            key={key}
+            llave={key}
+            item={this.props.items[key]}
+            updateItem={this.props.updateItem}
+            deleteItem={this.props.deleteItem}
+          />
+        ))}
+        <AddItemForm addItem={this.props.addItem} />
+        {/* <button onClick={this.loadItems}></button> */}
+      </div>
+    )
+  }
 }
